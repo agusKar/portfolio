aeb253628aced35b3ff62cd83c23f541dd6a7020
Agustin Karkocki <agustin.karkocki@gmail.com>
2022-08-20 14:14:29 -0300

    added EmailJs for email sends

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,39 @@
+import emailjs from "@emailjs/browser";
+import { FormEvent, useRef } from "react";
 const Contact = () => {
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: FormEvent) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_isc4lw7",
+        "template_enymegw",
+        form.current as HTMLFormElement,
+        "I9XHg_GAg0ChGzMzJ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="row">
       <div className="col-12 text-center">
-        <h1>Contact me</h1>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
       </div>
     </div>
   );
